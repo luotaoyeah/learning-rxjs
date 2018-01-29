1e7c532521e6bbe9a4de7915f4c75b597b699551
luotao <luotaoyeah@gmail.com>
2018-01-29 10:47:32 +0800

    doc: 01-01-04-03: observable execution: try/catch

---
+++ src/doc/01-overview/01-01-observable/01-01-04-anatomy-of-an-observable/01-01-04-03-executing-observables/01-01-04-03-02.ts
@@ -0,0 +1,43 @@
+/*
+ * Overview
+ *     Observable
+ *         Anatomy Of An Observable
+ *             Executing Observables
+ */
+
+import chalk from "chalk";
+import { Observable, Subscriber } from "rxjs";
+
+console.log(
+  chalk.red("\n-------------------------------------------------- 01")
+);
+{
+  /*
+   * 通常可以在 subscribe 函数中，通过 try/catch 捕获异常，
+   * 然后通过 subscriber.error() 方法，将错误抛出去
+   */
+
+  const observable = new Observable((subscriber: Subscriber<number>) => {
+    try {
+      subscriber.next(1);
+      subscriber.next(2);
+
+      /* 抛出一个异常 */
+      JSON.parse("");
+
+      subscriber.next(3);
+      subscriber.complete();
+    } catch (e) {
+      subscriber.error(e);
+    }
+  });
+
+  observable.subscribe(
+    (value: number) => {
+      console.log(chalk.red(value.toString()));
+    },
+    (error: Error) => {
+      console.log(chalk.grey.bgRed(error.message));
+    }
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./doc/01-overview/01-01-observable/01-01-04-anatomy-of-an-observable/01-01-04-03-executing-observables/01-01-04-03-01.ts";
+import "./doc/01-overview/01-01-observable/01-01-04-anatomy-of-an-observable/01-01-04-03-executing-observables/01-01-04-03-02.ts";
