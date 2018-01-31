65eca15c0854545ee60f7a284ea608c3628be243
luotao <luotaoyeah@gmail.com>
2018-01-31 19:57:12 +0800

    api: delay: until a given date

---
+++ src/doc/02-api/delay/02.ts
@@ -0,0 +1,23 @@
+/*
+ * API
+ *     delay
+ */
+
+import chalk from "chalk";
+import { interval } from "rxjs";
+import { delay } from "rxjs/operators";
+
+console.log(
+  chalk.red("\n-------------------------------------------------- 01"),
+);
+{
+  /*
+   * 将 source observable 的数据推送延迟到某个时间点
+   */
+
+  interval(1000)
+    .pipe(delay<number>(new Date(Date.now() + 3000)))
+    .subscribe((value: number) => {
+      console.log(chalk.red(value.toString()));
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./doc/02-api/delay/01.ts";
+import "./doc/02-api/delay/02.ts";
