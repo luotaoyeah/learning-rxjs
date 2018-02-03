eff833df6f23e516f9bd5ded312c11e7bf99e276
luotao <luotaoyeah@gmail.com>
2018-02-03 19:06:39 +0800

    feat(dissecting-rxjs): 02-03: TeardownLogic: Function

--- src/doc/01-overview/01-01-observable/01-01-04-anatomy-of-an-observable/01-01-04-04-disposing-observable-executions/01-01-04-04-02.ts
+++ src/doc/01-overview/01-01-observable/01-01-04-anatomy-of-an-observable/01-01-04-04-disposing-observable-executions/01-01-04-04-02.ts
@@ -9,28 +9,28 @@ import chalk from "chalk";
 import { Observable, Subscriber, Subscription } from "rxjs";
 
 console.log(
-  chalk.red("\n-------------------------------------------------- 01")
+  chalk.red("\n-------------------------------------------------- 01"),
 );
 {
   /*
-   * 我们可以通过调用 Subscription.unsubscribe() 方法终止 observable execution，
-   * 但是具体需要怎么去终止，以及需要怎么去释放资源，需要我们自己定义，
-   * 如何定义？通过在 subscribe 参数函数中，返回一个新的 function，
-   * 这个 function 里面包含的就是如何释放资源的逻辑，
-   * 当我们调用 Subscription.unsubscribe() 方法时，这个返回的 function 就会自动执行
+   * 我们可以通过调用 Subscription.unsubscribe() 方法终止 observable execution,
+   * 但是具体需要怎么去终止, 以及需要怎么去释放资源, 需要我们自己定义,
+   * 如何定义? 通过在 subscribe 参数函数中, 返回一个新的 function,
+   * 这个 function 里面包含的就是如何释放资源的逻辑,
+   * 当我们调用 Subscription.unsubscribe() 方法时, 这个返回的 function 就会自动执行
    */
 
   const observable = new Observable((subscriber: Subscriber<number>) => {
-    const interval = setInterval(() => {
+    const timer: number = setInterval(() => {
       subscriber.next(9);
     }, 1000);
 
     /*
-     * 在 subscribe 函数中，返回一个 dispose() 函数
+     * 在 subscribe 函数中, 返回一个 function
      */
-    return function dispose() {
+    return () => {
       console.log(chalk.red("DISPOSING"));
-      clearInterval(interval);
+      clearInterval(timer);
     };
   });
 
@@ -40,7 +40,7 @@ console.log(
 
   setTimeout(() => {
     /*
-     * 当我们调用 unsubscribe() 方法时，就会自动调用我们在 subscribe() 函数中返回的 dispose() 函数
+     * 当我们调用 unsubscribe() 方法时, 就会自动调用我们在 subscribe() 函数中返回的 function
      */
     subscription.unsubscribe();
   }, 5000);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./book/dissecting-rxjs/02/02-03/02-03-01.ts";
+import "./book/dissecting-rxjs/02/02-03/02-03-02.ts";
