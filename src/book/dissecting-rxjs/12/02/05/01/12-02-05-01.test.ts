e9d8c155a3ce9002b86b1ffe6b9680a61e7a3622
luotao <luotaoyeah@gmail.com>
2018-02-05 19:56:02 +0800

    feat(dissecting-rxjs): 12-02-05: marble test

--- src/book/dissecting-rxjs/12/02/05/01/12-02-05-01.test.ts
+++ src/book/dissecting-rxjs/12/02/05/01/12-02-05-01.test.ts
@@ -1,8 +1,9 @@
 import { TestScheduler } from "rxjs/testing";
 import { RunHelpers } from "rxjs/internal/testing/TestScheduler";
 import { ColdObservable } from "rxjs/internal/testing/ColdObservable";
+import { map } from "rxjs/operators";
 
-describe("12-02-01-01", () => {
+describe("12-02-05-01", () => {
   let scheduler: TestScheduler;
 
   beforeEach(() => {
@@ -17,9 +18,26 @@ describe("12-02-01-01", () => {
       const { cold, expectObservable } = helpers;
 
       const source$: ColdObservable<string> = cold("--a--b--|");
-      const expected: string = "--a--b--|";
+      const expectedMarbles: string = "--a--b--|";
 
-      expectObservable(source$).toBe(expected);
+      expectObservable(source$).toBe(expectedMarbles);
+    });
+  });
+
+  it("should map()", () => {
+    scheduler.run((helpers: RunHelpers) => {
+      const { cold, expectObservable } = helpers;
+
+      const source$: ColdObservable<number> = cold("--a--b--|", {
+        a: 1,
+        b: 2,
+      });
+      const expectedMarbles: string = "--a--b--|";
+
+      expectObservable(source$.pipe(map(i => i * 2))).toBe(expectedMarbles, {
+        a: 2,
+        b: 4,
+      });
     });
   });
 });
---
+++ src/book/dissecting-rxjs/12/02/05/01/12-02-05-01.ts
@@ -0,0 +1,19 @@
+/*
+ * 12.2.5 操纵时间的 TestScheduler
+ *     1. 弹珠测试
+ */
+
+import chalk from "chalk";
+
+console.log(
+  chalk.red("\n-------------------------------------------------- 02"),
+);
+{
+  /*
+   * rxjs 提供了 TestScheduler 用来对 rxjs 进行测试,
+   * 由于 TestScheduler 是继承自 AsyncScheduler,
+   * 因此只能对基于 AsyncScheduler 的 observable 进行测试
+   */
+}
+
+export {};
