17bccf84acc41f52acd3419949e7f8442a9453f5
luotao <luotaoyeah@gmail.com>
2018-02-06 16:10:22 +0800

    feat(dissecting-rxjs): 12-02-05-02: test interval()

--- src/book/dissecting-rxjs/12/02/05/02/12-02-05-02.test.ts
+++ src/book/dissecting-rxjs/12/02/05/02/12-02-05-02.test.ts
@@ -1,6 +1,7 @@
 import { TestScheduler } from "rxjs/testing";
 import { RunHelpers } from "rxjs/internal/testing/TestScheduler";
-import { EMPTY, NEVER, throwError } from "rxjs";
+import { EMPTY, interval, NEVER, Observable, throwError } from "rxjs";
+import { take } from "rxjs/operators";
 
 describe("12-02-05-02", () => {
   let scheduler: TestScheduler;
@@ -36,8 +37,8 @@ describe("12-02-05-02", () => {
 
   /*
    * | 表示 Subscriber.complete(),
-   * 如果只有一个 |, 表示 EMPTY,
-   * 即该 observable 不推送数据, 直接调用 complete() 接收
+   * 如果只有一个 |, 则表示 EMPTY,
+   * 即该 observable 不推送数据, 直接调用 complete() 结束
    */
   it("should work with |", () => {
     scheduler.run(({ expectObservable }: RunHelpers) => {
@@ -47,7 +48,7 @@ describe("12-02-05-02", () => {
 
   /*
    * # 表示 Subscriber.error(),
-   * 如果只有一个 #, 表示 throwError(),
+   * 如果只有一个 #, 则表示 throwError(),
    * 即该 observable 不推送数据, 直接抛出一个错误
    */
   it("should work with #", () => {
@@ -63,6 +64,28 @@ describe("12-02-05-02", () => {
   it("should work with never()", () => {
     scheduler.run(({ expectObservable }: RunHelpers) => {
       expectObservable(NEVER).toBe("-");
+    });
+  });
+
+  /*
+   * 如下, 对 interval() 产生的 observable 进行测试,
+   * 因为 interval() 产生的是一个永不结束的数据流,
+   * 我们需要使用 take() 来截取前四个数据
+   */
+  it("should work with interval()", () => {
+    scheduler.run(({ expectObservable }: RunHelpers) => {
+      const observable01: Observable<number> = interval(1).pipe(take(4));
+
+      /*
+       * marble string 中, 除了 | 和 #之外, 其他的字符都是会占用时间帧的,
+       * 如下, -abc 每个字符分别都会占用一个帧
+       */
+      expectObservable(observable01).toBe("-abc(d|)", {
+        a: 0,
+        b: 1,
+        c: 2,
+        d: 3,
+      });
     });
   });
 });
