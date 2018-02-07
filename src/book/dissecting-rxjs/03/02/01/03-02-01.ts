f456d9b8dce014cc4b54c8e75150508945411e76
luotao <luotaoyeah@gmail.com>
2018-02-07 20:01:27 +0800

    test(dissecting-rxjs): 03-02-01: operator types by functionality

---
+++ src/book/dissecting-rxjs/03/02/01/03-02-01.ts
@@ -0,0 +1,38 @@
+/*
+ * 3.2.1 功能分类
+ */
+
+function fn01(): void {
+  /*
+   * 根据功能, 操作符可以分成下面 9 类:
+   *
+   *     1. 创建类(creation)
+   *            如: of()
+   *
+   *     2. 转化类(transformation)
+   *            如: map()
+   *
+   *     3. 过滤类(filtering)
+   *            如: filter()
+   *
+   *     4. 合并类(combination)
+   *            如: merge()
+   *
+   *     5. 多播类(multicasting)
+   *            如:
+   *
+   *     6. 错误处理类(error handling)
+   *            如: catchError()
+   *
+   *     7. 辅助工具类(utility)
+   *            如: tap()
+   *
+   *     8. 条件分支类(conditional)
+   *            如:
+   *
+   *     9. 数学合计类(math&aggregate)
+   *            如:
+   */
+}
+
+export { fn01 };
