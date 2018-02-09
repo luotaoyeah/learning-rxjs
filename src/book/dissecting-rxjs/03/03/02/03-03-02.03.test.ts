4f8db8c4357971ee19ccb17fd3636ceac7ba0ff8
luotao <luotaoyeah@gmail.com>
2018-02-09 17:36:41 +0800

    test(dissecting-rxjs): 03-03-04: let

--- src/book/dissecting-rxjs/03/03/02/03-03-02.03.test.ts
+++ src/book/dissecting-rxjs/03/03/02/03-03-02.03.test.ts
@@ -1,7 +1,4 @@
-import { fn01 } from "./03-03-02.03";
-import { of } from "rxjs";
 import { TestScheduler } from "rxjs/testing";
-import { RunHelpers } from "rxjs/internal/testing/TestScheduler";
 
 describe("src/book/dissecting-rxjs/03/03/02/03-03-02.03.ts", () => {
   let scheduler: TestScheduler;
@@ -10,24 +7,6 @@ describe("src/book/dissecting-rxjs/03/03/02/03-03-02.03.ts", () => {
     // tslint:disable-next-line:no-any
     scheduler = new TestScheduler((actual: any, expected: any) => {
       expect(actual).toEqual(expected);
-    });
-  });
-
-  it("should work", () => {
-    scheduler.run(({ expectObservable }: RunHelpers) => {
-      const observable01$ = of(1, 2, 3).pipe<number>(
-        fn01<number, number>()((value: number) => value * 2),
-      );
-
-      expect(observable01$).toBeTruthy();
-
-      /*
-            expectObservable(observable01$).toBe("(abc|)", {
-              a: 2,
-              b: 4,
-              c: 6,
-            });
-      */
     });
   });
 });
---
+++ src/book/dissecting-rxjs/03/03/04/03-03-04.01.ts
@@ -0,0 +1,6 @@
+/*
+ * @deprecated
+ *
+ * 3.3.4 lettable/pipeable 操作符
+ *     1. let: 让它来做
+ */
