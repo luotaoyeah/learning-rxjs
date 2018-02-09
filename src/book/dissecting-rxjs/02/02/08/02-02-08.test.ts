3b71d42ff9502841a14f352e301ec573382c0306
luotao <luotaoyeah@gmail.com>
2018-02-09 17:35:30 +0800

    test(dissecting-rxjs): 02-02-08: deprecated Observable.subscribe()

--- src/book/dissecting-rxjs/02/02/08/02-02-08.test.ts
+++ src/book/dissecting-rxjs/02/02/08/02-02-08.test.ts
@@ -1,9 +1,21 @@
-import "./02-02-08";
+import { TestScheduler } from "rxjs/testing";
+import { RunHelpers } from "rxjs/internal/testing/TestScheduler";
+import { source$ } from "./02-02-08";
 
-/*
- * TODO
- */
 describe("src/book/dissecting-rxjs/02/02/08/02-02-08.ts", () => {
-  // tslint:disable-next-line:no-empty
-  test("01", () => {});
+  let scheduler: TestScheduler;
+
+  beforeEach(() => {
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it("should work", () => {
+    scheduler.run(({ expectObservable }: RunHelpers) => {
+      expectObservable(source$).toBe("(a|)", {
+        a: 9,
+      });
+    });
+  });
 });
--- src/book/dissecting-rxjs/02/02/08/02-02-08.ts
+++ src/book/dissecting-rxjs/02/02/08/02-02-08.ts
@@ -4,26 +4,23 @@ import { Observable, Subscriber } from "rxjs";
 /*
  * 2.2.8 Observer 的简单形式
  */
-console.log(
-  chalk.red("\n-------------------------------------------------- 01"),
-);
-{
-  const source$ = new Observable((subscriber: Subscriber<number>) => {
-    subscriber.next(9);
-    subscriber.complete();
-  });
+const source$ = new Observable((subscriber: Subscriber<number>) => {
+  subscriber.next(9);
+  subscriber.complete();
+});
 
-  /*
-   * 使用三个函数, 分别表示 next()/error()/complete() 函数,
-   * 这种方式已经被废弃了
-   */
-  source$.subscribe(
-    (value: number) => {
-      console.log(chalk.white(String(value)));
-    },
-    null,
-    () => {
-      console.log(chalk.white("DONE"));
-    },
-  );
-}
+/*
+ * 使用三个函数, 分别表示 next(), error(), complete() 函数,
+ * 这种方式已经被**废弃**了
+ */
+source$.subscribe(
+  (value: number) => {
+    console.log(chalk.white(String(value)));
+  },
+  null,
+  () => {
+    console.log(chalk.white("DONE"));
+  },
+);
+
+export { source$ };
