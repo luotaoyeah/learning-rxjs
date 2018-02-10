26d2ecd145eeb1883a9d04a3eccaf24882f01482
luotao <luotaoyeah@gmail.com>
2018-02-10 19:13:01 +0800

    feat(dissecting-rxjs): 03-03-04: tap

---
+++ src/book/dissecting-rxjs/03/03/04/03-03-04.03.ts
@@ -0,0 +1,12 @@
+/*
+ * 3.3.4 lettable/pipeable 操作符
+ *     3. 被迫改名的 pipeable 操作符
+ */
+
+/*
+ * 有 4 个旧的操作符由于跟 JS 中的保留字相同, 因此在新版本中进行了改名, 它们分别为:
+ * do: tap
+ * catch: catchError
+ * switch: switchAll
+ * finally: finalize
+ */
