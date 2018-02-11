9a475e39dcd4f36bbead2d0f37ce6f26d3b4f164
luotao <luotaoyeah@gmail.com>
2018-02-11 20:24:13 +0800

    feat(dissecting-rxjs): 04-02-06: empty, EMPTY

---
+++ src/book/dissecting-rxjs/04/02/06/04-02-06.01.test.ts
@@ -0,0 +1,32 @@
+import { TestScheduler } from "rxjs/testing";
+import { RunHelpers } from "rxjs/internal/testing/TestScheduler";
+import { EMPTY, empty } from "rxjs";
+
+describe("src/book/dissecting-rxjs/04/02/06/04-02-06.01.ts", () => {
+  let scheduler: TestScheduler;
+
+  beforeEach(() => {
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  /**
+   * empty 操作符用来产生一个直接 complete 的数据流, 不会推送任何数据
+   * @deprecated
+   */
+  it("should work", () => {
+    scheduler.run(({ expectObservable }: RunHelpers) => {
+      expectObservable(empty()).toBe("|");
+    });
+  });
+
+  /*
+   * empty 已经弃用, 推荐使用新的 EMPTY 常量来替代
+   */
+  it("should work with EMPTY", () => {
+    scheduler.run(({ expectObservable }: RunHelpers) => {
+      expectObservable(EMPTY).toBe("|");
+    });
+  });
+});
