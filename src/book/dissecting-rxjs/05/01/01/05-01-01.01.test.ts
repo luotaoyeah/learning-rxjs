3bfc3c9b3d23f71be7227cdca884292d3d56e721
luotao <luotaoyeah@gmail.com>
2018-02-14 19:21:44 +0800

    feat(dissecting-rxjs): 05-01-01: concat

--- src/book/dissecting-rxjs/05/01/01/05-01-01.01.test.ts
+++ src/book/dissecting-rxjs/05/01/01/05-01-01.01.test.ts
@@ -1,4 +1,6 @@
 import { TestScheduler } from "rxjs/testing";
+import { concat, interval, of } from "rxjs";
+import { take } from "rxjs/operators";
 
 describe("src/book/dissecting-rxjs/05/01/01/05-01-01.01.ts", () => {
   let scheduler: TestScheduler;
@@ -14,5 +16,22 @@ describe("src/book/dissecting-rxjs/05/01/01/05-01-01.01.ts", () => {
    */
   it("should work with Array.concat()", () => {
     expect([1, 2].concat([3, 4], [5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  /*
+   * concat() 操作符类似于数组的 concat() 方法, 用来将多个 observable 的数据依次连接起来组成一个新的 observable 对象
+   */
+  it("should work with concat()", () => {
+    scheduler.run(({ expectObservable }) => {
+      const source$ = concat(of(1, 2), of(3, 4), interval(1000).pipe(take(2)));
+      expectObservable(source$).toBe("(abcd) 994ms e 999ms (f|)", {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4,
+        e: 0,
+        f: 1,
+      });
+    });
   });
 });
