08dcb9a96e69b802439ee37266dfa8c7090ba596
luotao <luotaoyeah@gmail.com>
2018-02-16 19:06:10 +0800

    feat(dissecting-rxjs): 05-01-06: combineLatest: fromEvent

---
+++ src/book/dissecting-rxjs/05/01/06/05-01-06.02.ts
@@ -0,0 +1,20 @@
+/*
+ * 5.1.6 解决 glitch
+ */
+
+/*
+ * 使用获取鼠标点击位置的坐标为例, 来展示 combineLatest() 多重依赖导致的问题
+ */
+
+import { combineLatest, fromEvent, Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+const click$ = fromEvent<MouseEvent>(document.querySelector("body")!, "click");
+const x$ = click$.pipe(map(i => i.x));
+const y$ = click$.pipe(map(i => i.y));
+
+combineLatest([x$, y$])
+  .pipe(map(([x, y]) => `[${x}, ${y}]`))
+  .subscribe(value => {
+    console.log(value);
+  });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./book/dissecting-rxjs/04/03/04/04-03-04.01.ts";
+import "./book/dissecting-rxjs/05/01/06/05-01-06.02.ts";
