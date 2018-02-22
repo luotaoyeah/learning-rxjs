f52e78934be575517ce6ad938b339b08836cf9c3
luotao <luotaoyeah@gmail.com>
2018-02-22 16:15:18 +0800

    feat(dissecting-rxjs): 07-02-04: distinctUntilChanged

--- src/book/dissecting-rxjs/07/02/04/07-02-04.04.test.ts
+++ src/book/dissecting-rxjs/07/02/04/07-02-04.04.test.ts
@@ -1,8 +1,8 @@
 import { TestScheduler } from "rxjs/testing";
-import { concat, interval, of, timer } from "rxjs";
-import { distinct, mapTo } from "rxjs/operators";
+import { concat, of, timer } from "rxjs";
+import { distinct, distinctUntilChanged, mapTo } from "rxjs/operators";
 
-describe("src/book/dissecting-rxjs/07/02/04/07-02-04.03.ts", () => {
+describe("src/book/dissecting-rxjs/07/02/04/07-02-04.04.ts", () => {
   let scheduler: TestScheduler;
 
   beforeEach(() => {
@@ -12,8 +12,8 @@ describe("src/book/dissecting-rxjs/07/02/04/07-02-04.03.ts", () => {
   });
 
   /*
-   * distinct() 会在内部维护一个数据集合, 用来记录唯一的数据, 从而保证相同的数据只取一次,
-   * 我们可以通过参数, 让这个数据集合在指定的时机进行重置, 参数是一个 observable 对象, 记为 flushes$, 当 flushes$ 突出第一个数据时, 内部数据集合就会重置
+   * distinct() 是保证所有的数据不重复,
+   * distinctUntilChanged() 是保证本次的数据跟上次的数据不重复
    */
   it("should work", () => {
     scheduler.run(({ expectObservable }) => {
@@ -34,9 +34,16 @@ describe("src/book/dissecting-rxjs/07/02/04/07-02-04.03.ts", () => {
         { a: 0, b: 1, c: 2, d: 3 },
       );
 
-      expectObservable(source$.pipe(distinct(undefined, interval(3000)))).toBe(
+      expectObservable(source$.pipe(distinctUntilChanged())).toBe(
         "a 999ms b 1999ms c 999ms d 1999ms e 999ms f 999ms |",
-        { a: 0, b: 1, c: 2, d: 0, e: 1, f: 3 },
+        {
+          a: 0,
+          b: 1,
+          c: 2,
+          d: 0,
+          e: 1,
+          f: 3,
+        },
       );
     });
   });
