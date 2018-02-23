b72e6f2b829b588e8119ac5ad9c1ccb8ece8f277
luotao <luotaoyeah@gmail.com>
2018-02-23 17:12:10 +0800

    feat(dissecting-rxjs): 08-02-02: mapTo

---
+++ src/book/dissecting-rxjs/08/02/02/08-02-02.01.test.ts
@@ -0,0 +1,31 @@
+import { TestScheduler } from "rxjs/testing";
+import { interval } from "rxjs";
+import { mapTo, take } from "rxjs/operators";
+
+describe("src/book/dissecting-rxjs/08/02/02/08-02-02.01.ts", () => {
+  let scheduler: TestScheduler;
+
+  beforeEach(() => {
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  /*
+   * mapTo() 表示把每个上游数据都映射为指定的值
+   */
+  it("should work", () => {
+    scheduler.run(({ expectObservable }) => {
+      const source$ = interval(1000).pipe(
+        take(3),
+        mapTo("A"),
+      );
+
+      expectObservable(source$).toBe("1s a 999ms b 999ms (c|)", {
+        a: "A",
+        b: "A",
+        c: "A",
+      });
+    });
+  });
+});
