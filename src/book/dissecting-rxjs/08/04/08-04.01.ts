aaf47b7a6e0de1d8774d696fcda56f70995bcba6
luotao <luotaoyeah@gmail.com>
2018-02-25 10:40:51 +0800

    feat(dissecting-rxjs): 08-04-01: concatMap

--- src/book/dissecting-rxjs/08/04/08-04.01.ts
+++ src/book/dissecting-rxjs/08/04/08-04.01.ts
@@ -4,7 +4,7 @@
 
 /*
  * 普通的 map() 操作符, 接收一个 project 函数, 将上游的数据投射为一个新的数据, 再吐给下游,
- * 高阶的 map 操作符, 接收一个 project 函数, 但是这个 project 函数会将上游的数据投射为一个 observable 对象, 因此高阶的 map 操作符返回的是一个高阶的 observable 对象,
+ * 高阶的 map 系列操作符, 接收一个 project 函数, 但是这个 project 函数会将上游的数据投射为一个 observable 对象, 因此高阶的 map 操作符返回的是一个高阶的 observable 对象,
  *   之后再使用 concat/merge/switchAll/exhaust 四个操作符将这个高阶的 observable 再进行平化之后, 再吐给下游
  *
  * 高阶的 map 操作符有 4 个:
