b9bf3defb8cc2939881f3c0c64ec1da9043cf563
luotao <luotaoyeah@gmail.com>
2018-02-28 16:34:53 +0800

    feat(dissecting-rxjs): 10-04-01: multicast: selector

--- src/book/dissecting-rxjs/04/03/07/04-03-07.02.test.ts
+++ src/book/dissecting-rxjs/04/03/07/04-03-07.02.test.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:no-any */
 import { TestScheduler } from "rxjs/testing";
 import { repeatWhen, take } from "rxjs/operators";
 import { interval } from "rxjs";
@@ -12,26 +13,34 @@ describe("src/book/dissecting-rxjs/04/03/07/04-03-07.02.ts", () => {
   });
 
   /*
-   * repeatWhen() 的上游数据可以是异步数据
+   * repeatWhen() 的上游数据可以是异步数据, 记为 source$, repeatWhen() 的参数函数返回的 observable 对象记为 notifier$,
+   * 每当 notifier$ 吐出一个数据, 就会去订阅一次 source$, 从而产生一个 source execution,
+   * 当某个 source$ 完结时, 此时正在执行的其他的 source execution 都会完结,
+   * 当 notifier$ 完结时, 整个数据流就会完结
    */
   it("should work", () => {
     const source$ = interval(2000).pipe(take(2));
-    const notifier$ = interval(1000);
-    const repeatWhen$ = source$.pipe(repeatWhen(() => notifier$));
+
+    source$.subscribe = new Proxy(source$.subscribe, {
+      apply(target: any, thisArg: any, argArray?: any): any {
+        console.log("SUBSCRIBE");
+        return target.apply(thisArg, argArray);
+      },
+    });
 
     scheduler.run(({ expectObservable }) => {
       expectObservable(source$).toBe("2s a 1999ms (b|)", { a: 0, b: 1 });
 
-      /*
-       * TODO 这里吐出的数据为什么是这样的? 需要进一步研究
-       */
-      expectObservable(repeatWhen$.pipe(take(4))).toBe(
-        "2s a 1999ms b 2999ms c 999ms (d|)",
+      expectObservable(
+        source$.pipe(
+          repeatWhen(() => interval(1000)),
+          take(8),
+        ),
+      ).toBe(
+        "2000ms a 1999ms b 2999ms a 999ms a 999ms b 1999ms a 999ms a 999ms (b|)",
         {
           a: 0,
           b: 1,
-          c: 0,
-          d: 0,
         },
       );
     });
