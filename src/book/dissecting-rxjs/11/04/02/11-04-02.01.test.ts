4bf0f0bf4efcd4f1a2e228f04289a23d5a0e68df
luotao <luotaoyeah@gmail.com>
2018-03-01 16:03:29 +0800

    feat(dissecting-rxjs): 11-04-02: observeOn

---
+++ src/book/dissecting-rxjs/11/04/02/11-04-02.01.test.ts
@@ -0,0 +1,31 @@
+import { TestScheduler } from "rxjs/testing";
+import { asyncScheduler, range } from "rxjs";
+import { observeOn } from "rxjs/operators";
+
+describe("src/book/dissecting-rxjs/11/04/02/11-04-02.01.ts", () => {
+  let scheduler: TestScheduler;
+
+  beforeEach(() => {
+    // tslint:disable-next-line:no-any
+    scheduler = new TestScheduler((actual: any, expected: any) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  /*
+   * observeOn() 操作符, 用来根据上游的 observable 创建一个新的下游的 observable 对象,
+   * 这个新的 observable对象会使用指定的 scheduler 来控制数据的推送节奏
+   *
+   */
+  it("should work", () => {
+    scheduler.run(({ expectObservable }) => {
+      const source$ = range(1, 3);
+
+      expectObservable(source$.pipe(observeOn(asyncScheduler))).toBe("(abc|)", {
+        a: 1,
+        b: 2,
+        c: 3,
+      });
+    });
+  });
+});
