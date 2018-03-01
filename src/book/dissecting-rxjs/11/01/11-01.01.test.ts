16a77306b6b96b98294e9f5ccab99348e6492550
luotao <luotaoyeah@gmail.com>
2018-03-01 11:02:27 +0800

    feat(dissecting-rxjs): 11-03-03: asap/async/queue/animationFrame

--- src/book/dissecting-rxjs/11/01/11-01.01.test.ts
+++ src/book/dissecting-rxjs/11/01/11-01.01.test.ts
@@ -1,9 +1,11 @@
-import { range } from "rxjs";
+import {
+  animationFrameScheduler,
+  asapScheduler,
+  asyncScheduler,
+  queueScheduler,
+  range,
+} from "rxjs";
 import { TestScheduler } from "rxjs/testing";
-import { asap } from "rxjs/internal/scheduler/asap";
-import { async } from "rxjs/internal/scheduler/async";
-import { animationFrame } from "rxjs/internal/scheduler/animationFrame";
-import { queue } from "rxjs/internal/scheduler/queue";
 
 describe("src/book/dissecting-rxjs/11/01/11-01.01.ts", () => {
   let scheduler: TestScheduler;
@@ -16,7 +18,8 @@ describe("src/book/dissecting-rxjs/11/01/11-01.01.ts", () => {
   });
 
   /*
-   * scheduler 的作用是, 用来控制数据推送的节奏
+   * scheduler 的作用是, 用来控制数据推送的节奏,
+   * rxjs 中主要提供了 4 个 scheduler: asap/async/queue/animationFrame
    */
   it("should work", () => {
     scheduler.run(({ expectObservable }) => {
@@ -26,25 +29,25 @@ describe("src/book/dissecting-rxjs/11/01/11-01.01.ts", () => {
         c: 3,
       });
 
-      expectObservable(range(1, 3, asap)).toBe("(abc|)", {
+      expectObservable(range(1, 3, asapScheduler)).toBe("(abc|)", {
         a: 1,
         b: 2,
         c: 3,
       });
 
-      expectObservable(range(1, 3, async)).toBe("(abc|)", {
+      expectObservable(range(1, 3, asyncScheduler)).toBe("(abc|)", {
         a: 1,
         b: 2,
         c: 3,
       });
 
-      expectObservable(range(1, 3, queue)).toBe("(abc|)", {
+      expectObservable(range(1, 3, queueScheduler)).toBe("(abc|)", {
         a: 1,
         b: 2,
         c: 3,
       });
 
-      expectObservable(range(1, 3, animationFrame)).toBe("(abc|)", {
+      expectObservable(range(1, 3, animationFrameScheduler)).toBe("(abc|)", {
         a: 1,
         b: 2,
         c: 3,
---
+++ src/book/dissecting-rxjs/11/03/03/11-03-03.01.test.ts
@@ -0,0 +1,42 @@
+import { asapScheduler, asyncScheduler, queueScheduler } from "rxjs";
+
+describe("src/book/dissecting-rxjs/11/03/03/11-03-03.01.ts", () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it("should work", () => {
+    console.log("BEFORE");
+
+    /*
+     * asapScheduler 使用 microtask 队列来实现异步
+     */
+    asapScheduler.schedule(() => {
+      console.log("asap");
+    });
+
+    /*
+     * asyncScheduler 等价于使用 setTimeout() 来实现异步
+     */
+    asyncScheduler.schedule(() => {
+      console.log("async");
+    });
+
+    /*
+     * 对于 queueScheduler,
+     *   如果 delay 等于 0, 则使用同步的方式,
+     *   如果 delay 大于 0, 则跟 asyncScheduler 一样
+     */
+    queueScheduler.schedule(() => {
+      console.log("queue");
+    });
+
+    console.log("AFTER");
+
+    jest.advanceTimersByTime(1000);
+  });
+});
