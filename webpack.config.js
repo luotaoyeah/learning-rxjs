d2b36c9d95f4be743bfd028678e0605dfb9ced56
luotao <luotaoyeah@gmail.com>
2019-11-17 17:40:15 +0800

    feat: replace tslint with eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-const path = require("path");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = [
   {
-    entry: path.resolve("src/index.ts"),
+    entry: './src/index.ts',
     output: {
-      path: path.resolve("dist/"),
-      filename: "index.js",
+      filename: 'index.js',
+      path: path.resolve('dist/'),
     },
     module: {
       rules: [
         {
           test: /\.ts$/,
-          loader: "ts-loader",
+          loader: 'ts-loader',
           exclude: /node_modules/,
         },
       ],
     },
     resolve: {
-      extensions: [".ts", ".js", ".json"],
+      extensions: ['.ts', '.js', '.json'],
     },
-    target: "node",
-    mode: "development",
-    devtool: "eval-source-map",
+    target: 'node',
+    mode: 'development',
+    devtool: 'eval-source-map',
+    plugins: [new CleanWebpackPlugin()],
   },
 ];
