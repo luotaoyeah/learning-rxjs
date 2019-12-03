0f8954111bda87afd24f67ceac9a5130cad986cb
luotao <luotaoyeah@gmail.com>
2019-12-03 17:48:20 +0800

    refactor(dissecting-rxjs): unsubscribe observable

---
+++ src/book/dissecting-rxjs/02/03/01.test.ts
@@ -0,0 +1,43 @@
+import { TestScheduler } from 'rxjs/testing';
+import { source$ } from './01';
+
+describe('src/book/dissecting-rxjs/02/03/01.ts', () => {
+  let scheduler: TestScheduler;
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  beforeEach(() => {
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it('should work', () => {
+    scheduler.run(() => {
+      const actual: Array<number> = [];
+
+      // `Observable.subscribe()` 方法返回的是一个 `Subscription` 对象,
+      // 调用 `Subscription.unsubscribe()` 方法可以取消订阅,
+      // 取消订阅之后, observable 依然可以继续吐出数据, 但是 observer 不会再接收数据
+      const subscription = source$.subscribe({
+        next: (value: number) => {
+          actual.push(value);
+        },
+      });
+
+      setTimeout(() => {
+        subscription.unsubscribe();
+      }, 3500);
+
+      jest.advanceTimersByTime(1000 * 10);
+
+      expect(JSON.stringify(actual)).toEqual(JSON.stringify([1, 2, 3]));
+    });
+  });
+});
---
+++ src/book/dissecting-rxjs/02/03/01.ts
@@ -0,0 +1,12 @@
+// 2.3 退订 observer
+
+import { Observable, Subscriber } from 'rxjs';
+
+const source$: Observable<number> = new Observable((observer: Subscriber<number>) => {
+  let i = 0;
+  setInterval(() => {
+    observer.next(++i);
+  }, 1000);
+});
+
+export { source$ };
