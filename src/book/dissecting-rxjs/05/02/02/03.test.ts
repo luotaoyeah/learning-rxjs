d909dc1e5fe35dcd02f310e8fb3067e89c83b641
luotao <luotaoyeah@gmail.com>
2019-12-12 20:49:14 +0800

    refactor(dissecting-rxjs): zipAll

---
+++ src/book/dissecting-rxjs/05/02/02/03.test.ts
@@ -0,0 +1,33 @@
+import { TestScheduler } from 'rxjs/testing';
+import { interval } from 'rxjs';
+import { map, take, zipAll } from 'rxjs/operators';
+
+describe('src/book/dissecting-rxjs/05/02/02/03.ts', () => {
+  let scheduler: TestScheduler;
+
+  beforeEach(() => {
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  // 当 higher-order observable 完结之后, zipAll() 才能确定上游个数, 才会开始工作
+  it('should work', () => {
+    scheduler.run(({ expectObservable }) => {
+      const ho$ = interval(1000).pipe(
+        take(2),
+        map((i) =>
+          interval(500).pipe(
+            take(2),
+            map((j) => `${i}-${j}`),
+          ),
+        ),
+      );
+
+      expectObservable(ho$.pipe(zipAll())).toBe('2500ms a 499ms (b|) ', {
+        a: ['0-0', '1-0'],
+        b: ['0-1', '1-1'],
+      });
+    });
+  });
+});
