6578c6d4370da80f60a3d6e5a1ac409f2b8570f5
luotao <luotaoyeah@gmail.com>
2020-01-15 20:10:24 +0800

    refactor(dissecting-rxjs): publish

--- src/book/dissecting-rxjs/10/04/02/10-04-02.01.test.ts
+++ src/book/dissecting-rxjs/10/04/02/10-04-02.01.test.ts
@@ -1,7 +1,7 @@
-import { interval } from "rxjs";
-import { publish, refCount, take } from "rxjs/operators";
+import { interval } from 'rxjs';
+import { publish, refCount, take } from 'rxjs/operators';
 
-describe("src/book/dissecting-rxjs/10/04/02/10-04-02.01.ts", () => {
+describe('src/book/dissecting-rxjs/10/04/02/10-04-02.01.ts', () => {
   beforeAll(() => {
     jest.useFakeTimers();
   });
@@ -10,37 +10,36 @@ describe("src/book/dissecting-rxjs/10/04/02/10-04-02.01.ts", () => {
     jest.useRealTimers();
   });
 
-  /*
-   * publish() 实际上是一个语法糖, 底层使用的还是 multicast() 操作符
-   */
-  it("should work", () => {
-    const source$ = interval(1000).pipe(take(3));
-
+  // publish()         等价于 multicast(new Subject()),
+  // publish(selector) 等价于 multicast(() => new Subject(), selector)
+  it('should work', () => {
     const actual01: Array<number> = [];
     const actual02: Array<number> = [];
 
-    /*
-     * publish() 等价于 multicast(new Subject()),
-     * publish(selector) 等价于 multicast(() => new Subject(), selector)
-     */
-    const observable = source$.pipe(
-      publish(),
-      refCount(),
-    );
+    const source$ = interval(1000).pipe(take(3));
+    const observable = source$.pipe(publish(), refCount());
 
-    observable.subscribe(value => {
-      actual01.push(value);
+    const subscription01 = observable.subscribe({
+      next: (value) => {
+        actual01.push(value);
+      },
     });
 
     setTimeout(() => {
-      observable.subscribe(value => {
-        actual02.push(value);
+      subscription01.unsubscribe();
+    }, 2500);
+
+    setTimeout(() => {
+      observable.subscribe({
+        next: (value) => {
+          actual02.push(value);
+        },
       });
     }, 1500);
 
-    jest.advanceTimersByTime(3000);
+    jest.advanceTimersByTime(3500);
 
-    expect(actual01).toEqual([0, 1, 2]);
+    expect(actual01).toEqual([0, 1]);
     expect(actual02).toEqual([1, 2]);
   });
 });
