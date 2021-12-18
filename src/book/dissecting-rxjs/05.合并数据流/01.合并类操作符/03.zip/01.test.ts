8deb2ae6dd02f14c249a1eceec1b3f7ff48355a5
luotao <luotaoyeah@gmail.com>
2021-12-18 19:57:02 +0800

    refactor(dissecting-rxjs): 05.01.03 zip()

--- src/book/dissecting-rxjs/05.合并数据流/01.合并类操作符/03.zip/01.test.ts
+++ src/book/dissecting-rxjs/05.合并数据流/01.合并类操作符/03.zip/01.test.ts
@@ -2,7 +2,7 @@ import { TestScheduler } from 'rxjs/testing';
 import { interval, of, timer, zip } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 
-describe('src/book/dissecting-rxjs/05/01/03/01.ts', () => {
+describe('src/book/dissecting-rxjs/05.合并数据流/01.合并类操作符/03.zip/01.test.ts', () => {
   let scheduler: TestScheduler;
 
   beforeEach(() => {
@@ -13,7 +13,7 @@ describe('src/book/dissecting-rxjs/05/01/03/01.ts', () => {
 
   // zip() 表示拉链合并, 它会对上游的数据一一结对成为一个数组, 然后将数组吐给下游,
   // 由于不同的上游吐出数据的节奏不一样, 先吐出的数据需要等待后吐出的数据,
-  it('should work', () => {
+  it('01', () => {
     scheduler.run(({ expectObservable }) => {
       const source01$ = interval(1000);
       const source02$ = of('a', 'b', 'c');
@@ -30,7 +30,7 @@ describe('src/book/dissecting-rxjs/05/01/03/01.ts', () => {
 
   // 下游的数据个数, 是由 '数据个数最少的上游' 决定的, 记为 N,
   // 下游的完结时机, 是由 '最后吐出 N 个数据的上游' 决定的,
-  it('should work 02', () => {
+  it('02', () => {
     scheduler.run(({ expectObservable }) => {
       // 数据个数最少, 因此决定了下游的数据个数为 2,
       // 吐出 2 个数据最慢, 因此决定了下游的完结时机
