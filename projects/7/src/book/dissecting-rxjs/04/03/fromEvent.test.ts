3eae2708f6866a429f6490318910cfd5dec2016f
luotao <luotaoyeah@gmail.com>
2022-03-08 20:01:35 +0800

    refactor(7.dissecting-rxjs): 4.3.4 fromEvent

---
+++ projects/7/src/book/dissecting-rxjs/04/03/fromEvent.test.ts
@@ -0,0 +1,26 @@
+import { fromEvent, take } from 'rxjs';
+import { EventEmitter } from 'events';
+
+/**
+ * fromEvent() 将 DOM 事件或者 Node.JS 中的事件转换为 Observable.
+ */
+describe('fromEvent', () => {
+    it('01', (cb) => {
+        const eventEmitter = new EventEmitter();
+
+        fromEvent(eventEmitter, 'foo')
+            .pipe(take(3))
+            .subscribe({
+                next: (value) => {
+                    console.log(value);
+                },
+                complete: () => cb(),
+            });
+
+        eventEmitter.emit('foo', 1);
+        // bar 事件不会被监听
+        eventEmitter.emit('bar', 4);
+        eventEmitter.emit('foo', 2);
+        eventEmitter.emit('foo', 3);
+    });
+});
