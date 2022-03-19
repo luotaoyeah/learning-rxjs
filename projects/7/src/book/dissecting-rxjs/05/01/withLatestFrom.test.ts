9afbf89d9f8c2d0383b04811f7382f6cc44589ad
luotao <luotaoyeah@gmail.com>
2022-03-19 23:31:03 +0800

    refactor(7.dissecting-rxjs): 5.1.5 withLatestFrom

---
+++ projects/7/src/book/dissecting-rxjs/05/01/withLatestFrom.test.ts
@@ -0,0 +1,21 @@
+import { map, take, timer, withLatestFrom } from 'rxjs';
+import { log } from '../../util';
+
+describe('withLatestFrom', () => {
+    /**
+     * 跟 combineLatestWith() 的区别在于,
+     *   withLatestFrom()    只能由第一个上游控制吐数据的节奏, 其他上游只贡献数据,
+     *   combineLatestWith() 每个上游都可以控制吐数据的节奏
+     */
+    it('01', (cb) => {
+        const source01$ = timer(1000, 2000).pipe(map((i) => `A_${i}`));
+        const source02$ = timer(0, 1000).pipe(map((i) => `B_${i}`));
+
+        source01$.pipe(withLatestFrom(source02$), take(9)).subscribe({
+            next: (value) => {
+                log(value);
+            },
+            complete: () => cb(),
+        });
+    });
+});
