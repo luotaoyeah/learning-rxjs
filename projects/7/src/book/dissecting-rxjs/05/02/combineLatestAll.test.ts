f1e1f491098efbcdd2d222a2076096a9b41617a0
luotao <luotaoyeah@gmail.com>
2022-03-20 18:03:33 +0800

    refactor(7.dissecting-rxjs): 5.2.2 combineLatestAll

---
+++ projects/7/src/book/dissecting-rxjs/05/02/combineLatestAll.test.ts
@@ -0,0 +1,23 @@
+import { combineLatestAll, interval, map, take, zipAll } from 'rxjs';
+import { log } from '../../util';
+
+describe('combineLatestAll', () => {
+    it('01', (cb) => {
+        const ho$ = interval(1000).pipe(
+            take(2),
+            map((i) =>
+                interval(2000).pipe(
+                    take(2),
+                    map((j) => `${i}:${j}`),
+                ),
+            ),
+        );
+
+        ho$.pipe(combineLatestAll()).subscribe({
+            next: (value) => {
+                log(value);
+            },
+            complete: () => cb(),
+        });
+    });
+});
