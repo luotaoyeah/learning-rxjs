36b5e3ef5b55249dffbd2f3ab26a99176a2e220f
luotao <luotaoyeah@gmail.com>
2022-03-22 20:44:23 +0800

    refactor(7.dissecting-rxjs): 7.1.4 takeLast

---
+++ projects/7/src/book/dissecting-rxjs/07/01/takeLast.test.ts
@@ -0,0 +1,15 @@
+import { interval, take, takeLast, tap } from 'rxjs';
+import { log } from '../../util';
+
+describe('takeLast', () => {
+    it('01', (cb) => {
+        interval(1000)
+            .pipe(
+                take(5),
+                tap((value) => log('take():' + value)),
+                takeLast(3),
+                tap((value) => log('takeLast():' + value)),
+            )
+            .subscribe({ complete: () => cb() });
+    });
+});
