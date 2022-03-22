9a0db26f8996ae07f6001e864d7380c11842f3e4
luotao <luotaoyeah@gmail.com>
2022-03-22 20:20:14 +0800

    refactor(7.dissecting-rxjs): 7.1.2 first

---
+++ projects/7/src/book/dissecting-rxjs/07/01/first.test.ts
@@ -0,0 +1,44 @@
+import { first, interval, of } from 'rxjs';
+import { log } from '../../util';
+
+describe('first', () => {
+    /**
+     * 可以不传断言参数, 直接吐出上游的第一个数据.
+     */
+    it('01', (cb) => {
+        of(1, 2, 3)
+            .pipe(first())
+            .subscribe({
+                next: (value) => log(value),
+                complete: () => cb(),
+            });
+    });
+
+    /**
+     * 如果没有满足条件的数据, 则抛出一个错误.
+     */
+    it('02', (cb) => {
+        of(1, 2, 3)
+            .pipe(first((value) => value < 0))
+            .subscribe({
+                next: (value) => log(value),
+                error: (err) => {
+                    log(err.message);
+                    cb();
+                },
+                complete: () => cb(),
+            });
+    });
+
+    /**
+     * 可以指定默认值, 在没有满足条件的数据时使用.
+     */
+    it('03', (cb) => {
+        of(1, 2, 3)
+            .pipe(first((value) => value < 0, -1))
+            .subscribe({
+                next: (value) => log(value),
+                complete: () => cb(),
+            });
+    });
+});
