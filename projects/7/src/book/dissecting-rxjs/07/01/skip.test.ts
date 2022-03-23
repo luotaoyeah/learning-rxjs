4c72757218ea511433d0dc81021f4c371c3122ce
luotao <luotaoyeah@gmail.com>
2022-03-23 22:32:59 +0800

    refactor(7.dissecting-rxjs): 7.1.6 skip

--- projects/7/src/book/dissecting-rxjs/07/01/skip.test.ts
+++ projects/7/src/book/dissecting-rxjs/07/01/skip.test.ts
@@ -1,17 +1,21 @@
-import { interval, takeUntil, tap, timer } from 'rxjs';
+import { interval, skip, tap } from 'rxjs';
 import { log, logSubscribe } from '../../util';
 
-describe('takeUntil', () => {
+describe('skip', () => {
     it('01', (cb) => {
-        interval(2000)
+        interval(1000)
             .pipe(
                 logSubscribe(),
-                takeUntil(timer(3000)),
-                tap((value) => log('takeUntil():' + value)),
+                skip(3),
+                tap((value) => log(`skip(): ${value}`)),
             )
             .subscribe({
                 complete: () => {
                     log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
                     cb();
                 },
             });
--- projects/7/src/book/dissecting-rxjs/util.ts
+++ projects/7/src/book/dissecting-rxjs/util.ts
@@ -1,3 +1,5 @@
+import { Observable, OperatorFunction, skip, startWith, tap } from 'rxjs';
+
 /**
  * 打印日志, 包含当前的时分秒.
  */
@@ -11,4 +13,14 @@ function log(message: any): void {
     console.log(`[ ${new Date().toISOString().substring(11, 19)} ] ${msg}`);
 }
 
-export { log };
+/**
+ * 打印订阅上游的时间.
+ */
+function logSubscribe<T>(): OperatorFunction<T, T> {
+    return function (source: Observable<T>) {
+        log('SUBSCRIBE');
+        return source;
+    };
+}
+
+export { log, logSubscribe };
