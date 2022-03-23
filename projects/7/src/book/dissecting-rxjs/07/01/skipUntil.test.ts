dd6fefdfa423c0aacf36986f2f372f946fd19a49
luotao <luotaoyeah@gmail.com>
2022-03-23 23:22:52 +0800

    refactor(7.dissecting-rxjs): 7.1.7 skipUntil

---
+++ projects/7/src/book/dissecting-rxjs/07/01/skipUntil.test.ts
@@ -0,0 +1,23 @@
+import { interval, skipUntil, tap, timer } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('skipUntil', () => {
+    it('01', (cb) => {
+        interval(1000)
+            .pipe(
+                logSubscribe(),
+                skipUntil(timer(3000)),
+                tap((value) => log(`skipUntil(): ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
