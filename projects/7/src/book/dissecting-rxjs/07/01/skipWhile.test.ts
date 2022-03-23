eeec9c43f34d4a85372fb47ce4dd1eb9ae6389ee
luotao <luotaoyeah@gmail.com>
2022-03-23 23:20:36 +0800

    refactor(7.dissecting-rxjs): 7.1.7 skipWhile

---
+++ projects/7/src/book/dissecting-rxjs/07/01/skipWhile.test.ts
@@ -0,0 +1,23 @@
+import { interval, skipWhile, tap } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('skipWhile', () => {
+    it('01', (cb) => {
+        interval(1000)
+            .pipe(
+                logSubscribe(),
+                skipWhile((value) => value < 3),
+                tap((value) => log(`skipWhile(): ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
