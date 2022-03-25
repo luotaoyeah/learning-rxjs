5f553425c9b447ec260b014f30fe9aea2addb86e
luotao <luotaoyeah@gmail.com>
2022-03-25 21:16:04 +0800

    refactor(7.dissecting-rxjs): 7.2.1 throttle

---
+++ projects/7/src/book/dissecting-rxjs/07/02/throttle.test.ts
@@ -0,0 +1,24 @@
+import { interval, tap, throttle, timer } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('throttle', () => {
+    it('01', (cb) => {
+        interval(2000)
+            .pipe(
+                logSubscribe(),
+                tap((value) => log(`上游: ${value}`)),
+                throttle((value) => timer(3000)),
+                tap((value) => log(`throttle(): ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
