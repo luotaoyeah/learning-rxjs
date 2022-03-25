363ff0413ebf423633e854b83cb6164e1c9980cb
luotao <luotaoyeah@gmail.com>
2022-03-25 21:18:10 +0800

    refactor(7.dissecting-rxjs): 7.2.1 debounce

---
+++ projects/7/src/book/dissecting-rxjs/07/02/debounce.test.ts
@@ -0,0 +1,37 @@
+import { concat, debounce, interval, map, take, tap, timer } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('debounce', () => {
+    it('01', (cb) => {
+        concat(
+            interval(1000).pipe(
+                take(3),
+                map((i) => `A_${i}`),
+            ),
+            interval(3000).pipe(
+                take(2),
+                map((i) => `B_${i}`),
+            ),
+            interval(1000).pipe(
+                take(3),
+                map((i) => `C_${i}`),
+            ),
+        )
+            .pipe(
+                logSubscribe(),
+                tap((value) => log(`上游: ${value}`)),
+                debounce((value) => timer(2000)),
+                tap((value) => log(`debounce(): ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
