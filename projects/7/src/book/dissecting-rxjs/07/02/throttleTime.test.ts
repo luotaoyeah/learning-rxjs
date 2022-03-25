e9b085e7d919327c36ca62630c2939498cf64a0d
luotao <luotaoyeah@gmail.com>
2022-03-25 20:27:50 +0800

    refactor(7.dissecting-rxjs): 7.2.1 throttleTime: ThrottleConfig

--- projects/7/src/book/dissecting-rxjs/07/02/throttleTime.test.ts
+++ projects/7/src/book/dissecting-rxjs/07/02/throttleTime.test.ts
@@ -3,10 +3,14 @@ import { log, logSubscribe } from '../../util';
 
 describe('throttleTime', () => {
     /**
-     * 接收数据, 吐给下游, 开始计时, 在这个计时周期内, 所有上游数据都被丢弃.
+     * 默认配置: { leading: true, trailing: false }.
+     * 1. 接收数据, 吐给下游, 开始计时, 在计时周期内, 所有上游数据都被丢弃.
+     * 2. 重复 1.
+     *
+     * 由于 trailing: false, 计时周期是不连续的.
      */
     it('01', (cb) => {
-        interval(1000)
+        interval(2000)
             .pipe(
                 logSubscribe(),
                 tap((value) => log(`上游: ${value}`)),
@@ -26,15 +30,15 @@ describe('throttleTime', () => {
     });
 
     /**
-     * leading: true
-     *     在计时开始时, 将计时周期内的第一个上游数据吐给下游.
+     * { leading: false, trailing: true }.
+     * 1. 接收数据, 开始计时.
+     * 2. 计时结束, 将计时周期内最后一个数据吐给下游, 开始计时.
+     * 3. 重复 2.
      *
-     * trailing: true
-     *     在计时结束时, 将计时周期内的最后一个上游数据吐给下游.
-     *
+     * 由于 trailing: true, 数据吐给下游时, 会立即触发一个新的计时周期.
      */
     it('02', (cb) => {
-        interval(1000)
+        interval(2000)
             .pipe(
                 logSubscribe(),
                 tap((value) => log(`上游: ${value}`)),
@@ -57,10 +61,17 @@ describe('throttleTime', () => {
     });
 
     /**
-     * 接收数据, 吐给下游, 开始计时, 在这个计时周期内, 所有上游数据都被丢弃.
+     * { leading: true, trailing: true }.
+     * 1. 接收数据, 吐给下游, 开始计时.
+     * 2. 计时结束, 将计时周期内最后一个数据吐给下游, 开始计时.
+     * 3. 重复 2.
+     *
+     * 由于 trailing: true, 数据吐给下游时, 会立即触发一个新的计时周期.
+     *
+     * 与 { leading: false, trailing: true } 的唯一区别是: 是否吐出第一个上游数据.
      */
     it('03', (cb) => {
-        interval(1000)
+        interval(2000)
             .pipe(
                 logSubscribe(),
                 tap((value) => log(`上游: ${value}`)),
