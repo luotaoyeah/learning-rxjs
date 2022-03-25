96d8174ee7f99292cd8c49c66fe7d0ad241e49d9
luotao <luotaoyeah@gmail.com>
2022-03-25 21:49:02 +0800

    refactor(7.dissecting-rxjs): 7.2.3 sampleTime

---
+++ projects/7/src/book/dissecting-rxjs/07/02/sampleTime.test.ts
@@ -0,0 +1,27 @@
+import { interval, sampleTime, tap } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('sampleTime', () => {
+    /**
+     * 订阅后, 立即开启不间断的均匀计时周期, 在每个周期结束时, 吐出该周期内最后一个上游数据(如果有).
+     */
+    it('01', (cb) => {
+        interval(2000)
+            .pipe(
+                logSubscribe(),
+                tap((value) => log(`上游: ${value}`)),
+                sampleTime(3000),
+                tap((value) => log(`sampleTime(): ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
