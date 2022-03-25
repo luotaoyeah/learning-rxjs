2891118fc566126a81c3c1deb815658b13f007f3
luotao <luotaoyeah@gmail.com>
2022-03-25 21:31:02 +0800

    refactor(7.dissecting-rxjs): 7.2.2 audit

---
+++ projects/7/src/book/dissecting-rxjs/07/02/audit.test.ts
@@ -0,0 +1,24 @@
+import { audit, interval, tap, timer } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('audit', () => {
+    it('01', (cb) => {
+        interval(2000)
+            .pipe(
+                logSubscribe(),
+                tap((value) => log(`上游: ${value}`)),
+                audit((value) => timer(3000)),
+                tap((value) => log(`audit(): ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
