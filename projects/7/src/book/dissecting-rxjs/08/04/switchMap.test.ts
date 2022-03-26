c5ada6d11cf36240705a0636087aea2ae76480c2
luotao <luotaoyeah@gmail.com>
2022-03-26 23:27:04 +0800

    refactor(7.dissecting-rxjs): 8.4.3 switchMap

---
+++ projects/7/src/book/dissecting-rxjs/08/04/switchMap.test.ts
@@ -0,0 +1,28 @@
+import { interval, switchMap, take, tap } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('switchMap', () => {
+    /**
+     * switchMap = map + switchAll
+     */
+    it('01', (cb) => {
+        interval(1000)
+            .pipe(
+                take(2),
+                logSubscribe(),
+                tap((value) => log(`上游: ${value}`)),
+                switchMap((value) => interval(1000).pipe(take(3))),
+                tap((value) => log(`switchMap: ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
