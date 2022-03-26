417bd4b570c0f42f3f14231f2a3981cf8ad197b6
luotao <luotaoyeah@gmail.com>
2022-03-26 20:32:28 +0800

    refactor(7.dissecting-rxjs): 7.2.4 distinctUntilKeyChanged

---
+++ projects/7/src/book/dissecting-rxjs/07/02/distinctUntilKeyChanged.test.ts
@@ -0,0 +1,50 @@
+import { distinctUntilKeyChanged, of, tap } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('distinctUntilKeyChanged', () => {
+    /**
+     * 第 1 个参数 key 用来指定要比较哪个属性.
+     */
+    it('01', (cb) => {
+        of('1', 'a', '22', 'bb')
+            .pipe(
+                logSubscribe(),
+                tap((value) => log(`上游: ${value}`)),
+                distinctUntilKeyChanged('length'),
+                tap((value) => log(`distinctUntilKeyChanged(): ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+
+    /**
+     * 第 2 个参数 compare 用来指定比较方式.
+     */
+    it('02', (cb) => {
+        of({ age: 4, name: 'Foo1' }, { age: 7, name: 'Bar' }, { age: 5, name: 'Foo2' }, { age: 6, name: 'Foo3' })
+            .pipe(
+                logSubscribe(),
+                tap((value) => log(`上游: ${JSON.stringify(value)}`)),
+                distinctUntilKeyChanged('name', (x, y) => x.substring(0, 3) === y.substring(0, 3)),
+                tap((value) => log(`distinctUntilKeyChanged(): ${JSON.stringify(value)}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
