bd2646f179927b0cf79c93e7ec4fb5de959f8ccc
luotao <luotaoyeah@gmail.com>
2022-03-26 23:14:05 +0800

    refactor(7.dissecting-rxjs): 8.4.2 mergeMap

---
+++ projects/7/src/book/dissecting-rxjs/08/04/mergeMap.test.ts
@@ -0,0 +1,28 @@
+import { interval, mergeMap, take, tap } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('mergeMap', () => {
+    /**
+     * mergeMap = map + mergeAll
+     */
+    it('01', (cb) => {
+        interval(1000)
+            .pipe(
+                take(2),
+                logSubscribe(),
+                tap((value) => log(`上游: ${value}`)),
+                mergeMap((value) => interval(1000).pipe(take(3))),
+                tap((value) => log(`mergeMap: ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
