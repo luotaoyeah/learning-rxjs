9c3289b3fff542403a380347b8d74a9d69508f45
luotao <luotaoyeah@gmail.com>
2022-03-27 22:30:17 +0800

    refactor(7.dissecting-rxjs): 10.3.2 用 Subject 实现多播

--- projects/7/src/book/dissecting-rxjs/10/03/Subject.test.ts
+++ projects/7/src/book/dissecting-rxjs/10/03/Subject.test.ts
@@ -1,11 +1,11 @@
-import { Subject, tap } from 'rxjs';
+import { interval, Subject, take, tap } from 'rxjs';
 import { log, logSubscribe } from '../../util';
 
 describe('Subject', () => {
     /**
      * Subject 既是一个 Hot Observable, 又是一个 Observer.
      * Subject 类似于 EventEmitter.
-     * Subject 通过 Subject.currentObservers 属性记录了自己所有的订阅者.
+     * Subject 通过内部属性记录了自己所有的订阅者.
      */
     it('01', (cb) => {
         const subject = new Subject<number>();
@@ -30,5 +30,25 @@ describe('Subject', () => {
         subject.next(2);
         subject.next(3);
         subject.error(new Error('错'));
+    });
+
+    /**
+     * 让 Cold Observable 作为 Subject 的上游, 经过 Subject 之后就变成了一个 Hot Observable. 从而实现多播.
+     * 多播指的是: 多个 Observer 订阅了同一个 Observable, 这个 Observable 必须是一个 Hot Observable (比如 Subject).
+     */
+    it('02', (cb) => {
+        const subject = new Subject<number>();
+
+        // source$ 是要给 Cold Observable
+        const source$ = interval(1000).pipe(take(10));
+        // Subject 是一个 Observer (因为 Subject 拥有 next/complete/error 三个方法),
+        // 所以直接将一个 Subject 作为参数传给 subscribe() 方法
+        source$.subscribe(subject);
+
+        subject.pipe(tap((value) => log(`--| ${value}`))).subscribe();
+
+        setTimeout(() => {
+            subject.pipe(tap((value) => log(`----------| ${value}`))).subscribe();
+        }, 5000);
     });
 });
