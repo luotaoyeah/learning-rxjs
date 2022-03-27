ec2be823b707767952eed231d70dfe1506941f45
luotao <luotaoyeah@gmail.com>
2022-03-27 19:15:30 +0800

    refactor(7.dissecting-rxjs): 8.5 partition

---
+++ projects/7/src/book/dissecting-rxjs/08/05/partition.test.ts
@@ -0,0 +1,28 @@
+import { interval, map, merge, partition, tap } from 'rxjs';
+import { log, logSubscribe } from '../../util';
+
+describe('partition', () => {
+    /**
+     * 将一个 Observable 拆成两个 Observable, 然后放入一个数组.
+     * 第一个 Observable 中的数据都满足条件, 第二个 Observable 中的数据都不满足条件.
+     */
+    it('01', (cb) => {
+        const [even$, odd$] = partition(interval(1000), (value) => value % 2 === 0);
+
+        merge(even$.pipe(map((value) => `偶数: ${value}`)), odd$.pipe(map((value) => `奇数: ${value}`)))
+            .pipe(
+                logSubscribe(),
+                tap((value) => log(`上游: ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(e.message);
+                    cb();
+                },
+            });
+    });
+});
