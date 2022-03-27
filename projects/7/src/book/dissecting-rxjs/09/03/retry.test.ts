f5a8cb551a9b85a6e32303fafd7dff58e6377507
luotao <luotaoyeah@gmail.com>
2022-03-27 20:52:17 +0800

    refactor(7.dissecting-rxjs): 9.3.3 retryWhen [deprecated]

--- projects/7/src/book/dissecting-rxjs/09/03/retry.test.ts
+++ projects/7/src/book/dissecting-rxjs/09/03/retry.test.ts
@@ -1,8 +1,8 @@
-import { catchError, interval, map, of, retry, take, tap } from 'rxjs';
+import { catchError, interval, map, of, retry, take, tap, timer } from 'rxjs';
 import { log, logSubscribe } from '../../util';
 
 describe('retry', () => {
-    const source$ = interval(100).pipe(
+    const source$ = interval(1000).pipe(
         take(10),
         map((i) => {
             if (i === 4) {
@@ -12,15 +12,40 @@ describe('retry', () => {
         }),
     );
 
-    /**
-     *
-     */
     it('01', (cb) => {
         source$
             .pipe(
                 logSubscribe(),
                 tap((value) => log(`--| ${value}`)),
                 retry(2),
+                catchError((e, caught$) => of(8)),
+                take(20),
+                tap((value) => log(`----------| ${value}`)),
+            )
+            .subscribe({
+                complete: () => {
+                    log('COMPLETE');
+                    cb();
+                },
+                error: (e) => {
+                    log(`ERROR | ${e.message}`);
+                    cb();
+                },
+            });
+    });
+
+    /**
+     * 参数 RetryConfig.delay 表示延迟多久再重试, 而不是立即重试.
+     */
+    it('02', (cb) => {
+        source$
+            .pipe(
+                logSubscribe(),
+                tap((value) => log(`--| ${value}`)),
+                retry({
+                    count: 1,
+                    delay: () => timer(1000),
+                }),
                 catchError((e, caught$) => of(8)),
                 take(20),
                 tap((value) => log(`----------| ${value}`)),
