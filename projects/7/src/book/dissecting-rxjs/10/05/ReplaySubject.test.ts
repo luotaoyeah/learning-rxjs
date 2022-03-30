8b2bd5048046a89ea30eff587bd29a17c42227af
luotao <luotaoyeah@gmail.com>
2022-03-30 23:41:36 +0800

    refactor(7.dissecting-rxjs): 10.5.2 ReplaySubject

---
+++ projects/7/src/book/dissecting-rxjs/10/05/ReplaySubject.test.ts
@@ -0,0 +1,40 @@
+import { interval, map, ReplaySubject, share, Subject, take, tap } from 'rxjs';
+import { log } from '../../util';
+
+describe('ReplaySubject', () => {
+    /**
+     * ReplaySubject 会缓存上游数据, 可以指定只缓存最新的 N 个数据(否则缓存所有数据).
+     * 当有新的下游订阅时, 会将缓存的数据一次性吐给下游.
+     *
+     * 当上游完结之后, 下游再来订阅, 此时下游不会去订阅上游, 而是直接获得缓存的数据,
+     * 这样就避免了重新订阅上游之后, 又重新走一遍所有的管道.
+     */
+    it('01', (cb) => {
+        const cold$ = interval(1000).pipe(take(5));
+
+        const observable = cold$.pipe(
+            tap({
+                subscribe: () => log('SUBSCRIBE'),
+                next: (value) => log(`-----| ${value}`),
+            }),
+            share({
+                connector: () => new ReplaySubject(3),
+                resetOnError: false,
+                resetOnComplete: false,
+                resetOnRefCountZero: false,
+            }),
+        );
+
+        observable.subscribe({ next: (value) => log(`---------------| ${value}`) });
+
+        setTimeout(() => {
+            observable.subscribe({ next: (value) => log(`-------------------------| ${value}`) });
+        }, 1000 * 3);
+
+        setTimeout(() => {
+            // 上游完结了我再去订阅, 如果用的是 Subject, 则会重新订阅上游冷流,
+            // 如果用的是 ReplaySubject, 则不会订阅上游冷流, 而是直接一次性获得缓存数据.
+            observable.subscribe({ next: (value) => log(`-----------------------------------| ${value}`) });
+        }, 1000 * 7);
+    });
+});
